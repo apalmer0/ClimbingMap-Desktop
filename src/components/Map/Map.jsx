63aabc9305b4c604ce266991c3ef5b7e5574c577
Andrew Palmer <andrew.palmer.10@gmail.com>
2018-02-08 21:33:15 -0500

    Adds polygon and contains location

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { bool } from 'prop-types';
 import {
   Circle,
+  Polygon,
   DirectionsRenderer,
   GoogleMap,
   withGoogleMap,
@@ -20,6 +21,7 @@ const crgWatertown = {
   lat: 42.3696015,
   lng: -71.1985997,
 };
+const jsts = require('jsts');
 
 class Map extends Component {
   state = {
@@ -36,25 +38,67 @@ class Map extends Component {
       travelMode: google.maps.TravelMode.DRIVING,
     }, (result, status) => {
       if (status === google.maps.DirectionsStatus.OK) {
-        const polyline = result.routes[0].overview_polyline;
-        const decoded = google.maps.geometry.encoding.decodePath(polyline);
-        console.log('decoded', decoded);
-        const newArr = decoded.map((pts) => {
-          return ({
-            lat: pts.lat(),
-            lng: pts.lng(),
-          });
+        // const polyline = result.routes[0].overview_polyline;
+        // const decoded = google.maps.geometry.encoding.decodePath(polyline);
+        // console.log('decoded', decoded);
+        // const newArr = decoded.map((pts) => {
+        //   return ({
+        //     lat: pts.lat(),
+        //     lng: pts.lng(),
+        //   });
+        // });
+        // const shortArr = [];
+        // newArr.forEach((ele, idx) => {
+        //   if (idx % 10 === 0) {
+        //     shortArr.push(ele);
+        //   }
+        // });
+        // console.log('shortArr', shortArr);
+        const loc1 = {
+          lat: 42.380007,
+          lng: -71.1805682,
+        };
+        const loc2 = {
+          lat: 25.774,
+          lng: -80.19,
+        };
+        const locationAlongRoute = new google.maps.LatLng(loc1.lat, loc1.lng)
+        const carribean = new google.maps.LatLng(loc2.lat, loc2.lng)
+        const overviewPath = result.routes[0].overview_path;
+        const overviewPathGeo = [];
+        for (let i = 0; i < overviewPath.length; i++) {
+          overviewPathGeo.push(
+            [overviewPath[i].lng(), overviewPath[i].lat()],
+          );
+        }
+        console.log('result.routes[0]', result.routes[0]);
+        console.log('overviewPathGeo', overviewPathGeo);
+        const tenKm = 10 / 111.12;
+        const distance = tenKm / 50; // m
+        const geoInput = {
+          type: 'LineString',
+          coordinates: overviewPathGeo,
+        };
+        const geoReader = new jsts.io.GeoJSONReader();
+        const geoWriter = new jsts.io.GeoJSONWriter();
+        const geometry = geoReader.read(geoInput).buffer(distance);
+        const prePolygon = geoWriter.write(geometry);
+        const polygon = prePolygon.coordinates[0].map((array) => {
+          return {
+            lat: array[1],
+            lng: array[0],
+          };
         });
-        const shortArr = [];
-        newArr.forEach((ele, idx) => {
-          if (idx % 10 === 0) {
-            shortArr.push(ele);
-          }
-        });
-        console.log('shortArr', shortArr);
+        const bermudaTriangle = new google.maps.Polygon({paths: polygon});
+        const contain1 = google.maps.geometry.poly.containsLocation(locationAlongRoute, bermudaTriangle);
+        const contain2 = google.maps.geometry.poly.containsLocation(carribean, bermudaTriangle);
+
+        console.log('polygon', polygon);
+        console.log('should be true', contain1);
+        console.log('should be false', contain2);
         this.setState({
           directions: result,
-          shortArr,
+          polygon,
         });
       } else {
         console.error(`error fetching directions ${result}`);
@@ -68,20 +112,25 @@ class Map extends Component {
         defaultCenter={boston}
       >
         {this.state.directions && <DirectionsRenderer directions={this.state.directions} />}
-        {this.state.shortArr && this.state.shortArr.map((ele, idx) => {
-          return (
-            <Circle
-              key={idx}
-              center={new google.maps.LatLng(ele.lat, ele.lng)}
-              radius={400}
-            />
-          );
-        })}
+        {this.state.polygon &&
+          <Polygon
+            paths={this.state.polygon}
+          />
+        }
       </GoogleMap>
     );
   }
 }
 
+// {this.state.shortArr && this.state.shortArr.map((ele, idx) => {
+//   return (
+//     <Circle
+//       key={idx}
+//       center={new google.maps.LatLng(ele.lat, ele.lng)}
+//       radius={400}
+//     />
+//   );
+// })}
 Map.defaultProps = {
   isMarkerShown: false,
 };
